9a7ca1b138ca917b0322e548337b71f8062063c0
catevan <37692007+catevan@users.noreply.github.com>
2024-11-01 16:59:49 +0800

    Update qidian.js

--- qidian.js
+++ qidian.js
@@ -1,3 +1,4 @@
+[Script]
 cron "30 10 * * *" script-path=https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.js, timeout=10, enabled=false, tag=起点读书
 http-request https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v1\/video\/adv\/finishWatch script-path=https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.cookie.js, requires-body=true, timeout=10, tag=qidian.cookie
 http-response https\:\/\/h5\.if\.qidian\.com\/argus\/api\/v1\/video\/adv\/mainPage script-path=https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/qidian/qidian.taskId.js, requires-body=true, timeout=10, tag=qidian.taskId
