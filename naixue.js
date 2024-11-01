e859df394b0c3bdef1bb8f24f7a38996286f5851
catevan <37692007+catevan@users.noreply.github.com>
2024-11-01 17:17:06 +0800

    Update naixue.js

--- naixue.js
+++ naixue.js
@@ -16,8 +16,8 @@ CaptureCookie = switch, false, true, tag = 捕获Cookie, desc = 此开关控制�
 [Script]
 http-request ^https:\/\/tm-web\.pin-dao\.cn\/user\/base-userinfo script-path=https://gist.githubusercontent.com/Sliverkiss/4d0e9572b99530b7cb0e7298622aa2a9/raw/naixue.js, requires-body=true, timeout=60, enable = {CaptureCookie},tag=奈雪点单获取token
 # 定时签到
-cron "0,9 * * *" script-path=https://gist.githubusercontent.com/Sliverkiss/4d0e9572b99530b7cb0e7298622aa2a9/raw/naixue.js,timeout=300, tag=奈雪的茶
-cron "30 10 * * *" script-path=https://raw.githubusercontent.com/catevan/js/refs/heads/main/dlm.js,timeout=300, tag=达美乐披萨,img-url = https://raw.githubusercontent.com/Sliverkiss/QuantumultX/main/icon/Dlm.png
+
+cron "30 10 * * *" script-path=https://gist.githubusercontent.com/Sliverkiss/4d0e9572b99530b7cb0e7298622aa2a9/raw/naixue.js,timeout=300, tag=奈雪的茶
 [MITM]
 
 [MITM]
