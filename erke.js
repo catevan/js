a4d4da7140d3da25bd9d1ebfa503fcb352a9ee9f
catevan <37692007+catevan@users.noreply.github.com>
2024-11-25 22:04:57 +0800

    Update erke.js

--- erke.js
+++ erke.js
@@ -1,6 +1,13 @@
+[Argument]
+CaptureCookie = switch, false, true, tag = 捕获Cookie, desc = 此开关控制插件是否捕获Cookie
 [MITM]
 hostname = hope.demogic.com
 
+[Script]
+
+http-request ^https:\/\/hope\.demogic\.com\/gic-wx-app\/get-member-asset\.json script-path=https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js, enable = {CaptureCookie},tag=鸿星尔克Cookie
+
 cron "15 9 * * *" script-path=https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js, tag=鸿星尔克
 
-http-request ^https:\/\/hope\.demogic\.com\/gic-wx-app\/get-member-asset\.json script-path=https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js, tag=鸿星尔克Cookie
+[MITM]
+hostname = hope.demogic.com
